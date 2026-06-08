04ec1c16e116ccbd11c5198f375e4bf17ab90a87
YassenMohamed86 <mizerx86@gmail.com>
2026-06-08 17:11:21 +0300

    Delete tigr-tas directory

--- tigr-tas/backend/app/database/session.py
+++
@@ -1,7 +0,0 @@
-from typing import AsyncGenerator
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.database.engine import AsyncSessionLocal
-
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with AsyncSessionLocal() as session:
-        yield session
--- tigr-tas/backend/app/dependencies.py
+++
@@ -1,3 +0,0 @@
-# Dependencies
-from app.database.session import get_async_session
-from app.config.settings import get_settings
